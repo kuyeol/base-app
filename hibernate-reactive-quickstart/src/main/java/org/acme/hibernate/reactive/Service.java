731ef6e369cc5e1752fc17c0104f7e83ed52ccab
kuyeol <125532184+kuyeol@users.noreply.github.com>
2024-07-28 23:26:44 +0900

    A

---
+++ hibernate-reactive-quickstart/src/main/java/org/acme/hibernate/reactive/Service.java
@@ -0,0 +1,9 @@
+package org.acme.hibernate.reactive;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.hibernate.SessionFactory;
+
+public class Service {
+
+}
