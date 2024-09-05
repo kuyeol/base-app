382d85d1648ca736796436090e8844f188c59d76
kuyeol <125532184+kuyeol@users.noreply.github.com>
2024-09-05 21:58:20 +0900

    dd

---
+++ kafka-quickstart-consumer/src/main/java/org/acme/kafka/UserProfile.java
@@ -0,0 +1,16 @@
+package org.acme.kafka;
+
+import io.quarkus.mongodb.panache.MongoEntity;
+import org.bson.types.ObjectId;
+@MongoEntity(collection = "userprofile")
+public class UserProfile {
+
+public ObjectId id;
+public String name;
+public String email;
+public String phone;
+public String state;
+
+
+
+}
